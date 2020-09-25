1c763559473f916de028874d74bd956be17ab22c
ruwanepi <ruwan.epi@gmail.com>
2020-09-25 11:19:23 -0400

    3

--- Branching model 2.R
+++ Branching model 2.R
@@ -44,14 +44,14 @@ pop.cover      <- 0.8  #Coverage at the population level
 cati.efficacy      <- 1-((1-antibiotic.eff)*(1-water.tx.eff)*(1-water.store.eff))
 # Estimated impact within first week is to knock down R0 to 0.998
 cati.effectiveness <- pop.cover*cati.efficacy
-re.cati.0.6.d      <- r.0*(1-(cati.effectiveness))
+RE.novacc      <- r.0*(1-(cati.effectiveness))
 
 # Impact (effected on 7th day; initial). Include impact from vaccination.
 # Estimated reduction of R0 by CATI without vaccination results in Re=0.97
 cati.vacc.efficacy      <-  1-((1-vacc.2m.eff)*(1-antibiotic.eff)*(1-water.tx.eff)*(1-water.store.eff))
 # Estimated reduction of R0 by CATI with vaccination results in Re=0.67
 cati.vacc.effectiveness <- pop.cover*cati.vacc.efficacy
-re.cati.7.d             <- r.0*(1-(cati.vacc.effectiveness))
+RE.withvacc             <- r.0*(1-(cati.vacc.effectiveness))
   
 
 ######################################
@@ -59,9 +59,9 @@ re.cati.7.d             <- r.0*(1-(cati.vacc.effectiveness))
 ######################################
 
 set.seed(123)
-runs          <- 1000
-delay.distr   <- rtruncnorm(n=1000, a=1, b=6, mean=3, sd=2) 
+runs          <- 200
 # distribution of delays that can be randomly selected for each run
+delay.distr   <- rtruncnorm(n=200, a=1, b=6, mean=3, sd=2) 
 seed          <- 5
 
 # Simulate outbreak trajectory over a 30-day period (maximum 1000 cases per ring)
@@ -76,19 +76,20 @@ cols <- sample(viridis(runs))
 total.cases <- integer(runs) # set frame with 1000 runs 
 #total.cases <- data.frame(integer(runs), delay.distr)
 
-# *** How do I include delay.distr? I would like to iterate each run over the 
-# corresponding value from the delay.distr to force a delay on CATI implementation*** 
+# *** How do I include delay.distr? 
+# I would like to iterate each run over the corresponding value 
+# from delay.distr to force a delay on CATI implementation
 
-for(i in 1:runs) {
+for(i in 1:runs) { # Am I missing delay.distr in for loop?
   cases <- seed
   t <- rep(0,seed)
-  times <- t # Am I missing a delay statement here?
+  times <- t  
   
-while(cases > 0) # Am I missing a delay statement here?
+while(cases > 0) # Am I missing delay.distr in while loop?
   
   if (length(times)<2) # Use different Re values as CATI is implemented. Here it's R0.
     {secondary <- rnbinom(cases,size=k,mu=r.0)
-    t.new <- numeric() + delay.distr
+    t.new <- numeric()
     for(j in 1:length(secondary)) {
       t.new <- c(t.new,t[j] + rgamma(secondary[j],shape=ser.int.shape[1],
                                      scale=(1/ser.int.rate)))
@@ -98,7 +99,7 @@ while(cases > 0) # Am I missing a delay statement here?
     times <- c(times[times<31],t.new)
   } 
   else if (length(times)>1 & length(times)<7) { #Here it's Re with CATI (no OCV protection)
-    secondary <- rnbinom(cases,size=k,mu=re.cati.0.6.d)
+    secondary <- rnbinom(cases,size=k,mu=RE.novacc)
     t.new <- numeric()
     for(j in 1:length(secondary)) {
       t.new <- c(t.new,t[j] + rgamma(secondary[j],shape=ser.int.shape[1],
@@ -109,7 +110,7 @@ while(cases > 0) # Am I missing a delay statement here?
     times <- c(times[times<31],t.new)
   } 
   else if (length(times)>6 & length(times)<31){ # Here it's Re with CATI (and OCV protection)
-    secondary <- rnbinom(cases,size=k,mu=re.cati.7.d)
+    secondary <- rnbinom(cases,size=k,mu=RE.withvacc)
     t.new <- numeric()
     for(j in 1:length(secondary)) {
       t.new <- c(t.new,t[j] + rgamma(secondary[j],shape=ser.int.shape[1],
@@ -143,8 +144,8 @@ plnorm(10, lsm, lssd)
 plnorm(20, lsm, lssd)
 
 # Create dataframe with final epidemic size and incidence across 200 runs
-# ***I want to have another column for the delay used***
 # Incidence = (epidemic size/500 population) * 1000
+# ***Add another column for the delay used
 cases.df <- data.frame("run.num"=1:200, "symp.chol.cases.30d" = total.cases,
                        "chol.inc.1000.30d" = (total.cases/500)*1000)
 View(cases.df)
